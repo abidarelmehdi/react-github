2aeb8329c8bb9d2e041fe2a7510112d648556869
Abidar El Mehdi <abidar.mehdi95@gmail.com>
2020-04-07 19:52:31 +0100

    Updates

---
+++ precache-manifest.42dc7ebfe4b9ba68ba4dc04d7e84e9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727166f448573dea6613ab1d83f116ad",
+    "url": "/react-github/index.html"
+  },
+  {
+    "revision": "3293ff2e7860f8f68bc7",
+    "url": "/react-github/static/css/main.4f5a6001.chunk.css"
+  },
+  {
+    "revision": "32ba02db9085babb5266",
+    "url": "/react-github/static/js/2.d443e610.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/react-github/static/js/2.d443e610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3293ff2e7860f8f68bc7",
+    "url": "/react-github/static/js/main.61c7f1ec.chunk.js"
+  },
+  {
+    "revision": "96538a41227042b6506e",
+    "url": "/react-github/static/js/runtime-main.1340e935.js"
+  }
+]);
